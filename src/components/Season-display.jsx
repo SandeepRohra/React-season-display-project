7fa278912ac52be7eacfd77fce29eaf5140637b1
Sandeep Rohra <sandeep12rohra@gmail.com>
2022-01-08 21:37:19 +0530

    Notes added regarding to doubt

--- src/components/Season-display.jsx
+++ src/components/Season-display.jsx
@@ -10,6 +10,7 @@ const seasonConfig = {
   },
 }
 
+console.log(seasonConfig)
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? 'Summer' : 'Winter'
@@ -22,7 +23,7 @@ const SeasonDisplay = (props) => {
   // yaha p season ka output h winter kse props.lat is >0 and today month is jan less then 2
   let season = getSeason(props.lat, new Date().getMonth())
   //to ab seasonConfig m season is"Winter" then yaha Winter ka text aur iconName aajayega
-  const { text, iconName } = seasonConfig['Winter']
+  const { text, iconName } = seasonConfig[season]
   console.log(season)
 
   //   console.log(text)
